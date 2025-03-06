f72fb980834ec7e58a0f5f37cfb56a7ff750fd4e
Daniel Kesselberg <mail@danielkesselberg.de>
2025-03-06 12:51:08 +0100

    feat: add example for flight reservation
    Signed-off-by: Daniel Kesselberg <mail@danielkesselberg.de>

---
+++ itinerary_event.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+use Spatie\SchemaOrg\Schema;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$today = new DateTime();
+$today->setTime(14, 0, 0);
+
+$person = Schema::person()
+    ->name('Alice');
+$location = Schema::virtualLocation()
+    ->name('Talk');
+$event = Schema::event()
+    ->name('Itinerary brainstorming')
+    ->startDate((clone $today)->add(new DateInterval('P1D')))
+    ->location($location);
+$eventReservation = Schema::eventReservation()
+    ->reservationId('3000-1')
+    ->reservationStatus(\Spatie\SchemaOrg\ReservationStatusType::ReservationConfirmed)
+    ->underName($person)
+    ->reservationFor($event);
+
+$script = $eventReservation->toScript();
+
+$contents = '<html><meta http-equiv="content-type" content="text/html; charset=UTF-8"><body>Hi, lets do some itinerary brainstorming'
+    . PHP_EOL . $script . '</body><html>';
+
+$basePart = new Horde_Mime_Part();
+$basePart->setType('text/html');
+$basePart->setCharset('UTF-8');
+$basePart->setContents($contents);
+$basePart->setDescription('HTML Version of Message');
+
+/*
+ * To add the Mime-Version-Header
+ */
+$basePart->isBasePart(true);
+
+$mail = new Horde_Mime_Mail();
+$mail->addHeaders([
+    'From' => 'alice@test.local',
+    'To' => 'jane@doe.local',
+    'Subject' => 'Itinerary brainstorming',
+]);
+$mail->setBasePart($basePart);
+
+$transport = new Horde_Mail_Transport_Smtphorde([
+    'localhost' => 'localhost',
+    'host' => 'localhost',
+    'port' => 1025,
+    'username' => 'alice@test.local',
+    'password' => 'alice',
+    'secure' => false
+]);
+
+$mail->send($transport, false, false);
---
+++ itinerary_train.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+use Spatie\SchemaOrg\Schema;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$today = new DateTime();
+$today->setTime(14, 0, 0);
+
+$departureStation = Schema::trainStation()
+    ->name('Berlin');
+$arrivalStation = Schema::trainStation()
+    ->name('Paris');
+$trainTrip = Schema::trainTrip()
+    ->departureStation($departureStation)
+    ->departureTime((clone $today)->add(new DateInterval('P1D')))
+    ->arrivalStation($arrivalStation)
+    ->arrivalTime((clone $today)->add(new DateInterval('P1DT2H')));
+$trainReservation = Schema::trainReservation()
+    ->reservationId('2000-1')
+    ->reservationStatus(\Spatie\SchemaOrg\ReservationStatusType::ReservationConfirmed)
+    ->reservationFor($trainTrip);
+
+$script = $trainReservation->toScript();
+
+$contents = '<html><meta http-equiv="content-type" content="text/html; charset=UTF-8"><body>Hi, enjoy your trip from Berlin to Paris!'
+    . PHP_EOL . $script . '</body><html>';
+
+$basePart = new Horde_Mime_Part();
+$basePart->setType('text/html');
+$basePart->setCharset('UTF-8');
+$basePart->setContents($contents);
+$basePart->setDescription('HTML Version of Message');
+
+/*
+ * To add the Mime-Version-Header
+ */
+$basePart->isBasePart(true);
+
+$mail = new Horde_Mime_Mail();
+$mail->addHeaders([
+    'From' => 'alice@test.local',
+    'To' => 'jane@doe.local',
+    'Subject' => 'Train trip from Berlin to Paris',
+]);
+$mail->setBasePart($basePart);
+
+$transport = new Horde_Mail_Transport_Smtphorde([
+    'localhost' => 'localhost',
+    'host' => 'localhost',
+    'port' => 1025,
+    'username' => 'alice@test.local',
+    'password' => 'alice',
+    'secure' => false
+]);
+
+$mail->send($transport, false, false);
